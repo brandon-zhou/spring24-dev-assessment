4ca1fc60547b22b5b44f07f8298cc33d0f17fd84
brandon-zhou <57579343+brandon-zhou@users.noreply.github.com>
2024-01-17 02:11:17 -0500

    user changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,76 +1,95 @@
-import axios from 'axios';
-import React, { useCallback, useEffect, useState } from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import UserModal from "./components/UserModal";
+import { Button, ChakraProvider } from "@chakra-ui/react"
+
 // const data2 = [
 //   { name: "Anom", age: 19, gender: "Male" },
 //   { name: "Megha", age: 19, gender: "Female" },
 //   { name: "Subham", age: 25, gender: "Male" },
 // ]
 function App() {
-  const [data, setData] = useState<any[]>([])
+  const [data, setData] = useState<any[]>([]);
+
   // https://jsonplaceholder.typicode.com/users
-// http://localhost:4000/api/bog/users
+  // http://localhost:4000/api/bog/users
   const fetchData = useCallback(async () => {
-    axios.get("http://localhost:4000/api/bog/users").then((res) => setData(res.data));
+    axios
+      .get("http://localhost:4000/api/bog/users")
+      .then((res) => setData(res.data));
     // console.log(data);
   }, []);
   useEffect(() => {
     fetchData();
-  }, [fetchData])
+  }, []);
 
-  // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then((res) => res.json())
-  //   .then(d => setData(d))
-  //   .catch((err) => console.log(err))
-  // })
+  // console.log("hello");
+  // console.log(data);
 
-  console.log("hello");
-  console.log(data);
   return (
-    <div className="App">
-
-      {/* <ul>
-        {data.map((list, index) => (
-          <li key = {index} > {list.id} | {list.name}</li>
-        ))}
-      </ul> */}
-      <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Picture</th>
-                    <th>Phone</th>
-                    <th>Email</th>
-                    <th>Rating</th>
-                    <th>Status</th>
-                    <th>Hero Project</th>
-                </tr>
-                {data.map((item, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{item.name}</td>
-                            <td>{<img 
-      src={item.avatar}
-      alt="new"
-      />}</td>
-                            <td>{item.phone}</td>
-                            <td>{item.email}</td>
-                            <td>{item.rating}</td>
-                            <td>{item.status ? "active" : "inactive"}</td>
-                            <td>{item.hero_project}</td>
-                            <button onClick = {() => {alert(key);
-                                  const temp = [...data];
-                                  temp.splice(key,1);
-                                  setData(temp); console.log(data)}}>Delete</button>
-
-                        </tr>
-                    )
-                })}
-            </table>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Picture</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Rating</th>
+            <th>Status</th>
+            <th>Hero Project</th>
+            <UserModal userkey={null} user={null} data={data} setData={setData} update={false}></UserModal>
+            {/* <Button marginBottom={5}>New Entry</Button> */}
+            {/* <Link to="/">Home</Link> */}
+          </tr>
+          {data.map((item, key) => {
+            const num = key;
+            return (
+              // <tr key={key} onClick = {()=>alert(key)}>
+              <tr key={key}>
+                <td>{item.name}</td>
+                <td>{<img src={item.avatar} alt="new" />}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+                <td>{item.rating}</td>
+                <td>{item.status ? "active" : "inactive"}</td>
+                <td>{item.hero_project}</td>
+                <Button
+                  onClick={() => {
+                    const temp = [...data];
+                    temp.splice(key, 1);
+                    setData(temp);
+                  }}
+                  marginBottom={5}
+                >
+                  Delete
+                </Button>
+                <UserModal
+                  userkey={key}
+                  user={item}
+                  data={data}
+                  setData={setData}
+                  update={true}
+                />
+              </tr>
+            );
+          })}
+        </table>
+        <hr />
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
-
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
 
 export default App;
---
+++ frontend/src/components/UserModal.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+  
+  export default function UserModal({userkey, user, data, setData, update} : {userkey: any, user: any, data: any, setData: any, update: Boolean}) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [name, setName] = useState(user?.name || "");
+    const [avatar, setAvatar] = useState(user?.avatar || "");
+    const [phone, setPhone] = useState(user?.phone || "");
+    const [email, setEmail] = useState(user?.email || "");
+    const [rating, setRating] = useState(user?.rating || false);
+    const [status, setStatus] = useState(user?.status || "");
+    const [project, setProject] = useState(user?.hero_project || "");
+    const handleSubmit = () => {
+      if (userkey === null) {
+        let temp = {
+          name: name,
+          avatar: avatar,
+          phone: phone,
+          email: email,
+          rating: rating,
+          status: status,
+          hero_project: project,
+        }
+        setData([...data, temp]);
+      } else {
+        let dataTemp = [...data];
+        let temp = {...data[userkey]};
+        temp.name = name;
+        temp.avatar = avatar;
+        temp.phone = phone;
+        temp.email = email;
+        temp.rating = rating;
+        temp.status = status;
+        temp.hero_project = project;
+        dataTemp[userkey] = temp;
+        setData(dataTemp);
+      }
+    };
+
+    return (
+      <>
+        <Button onClick={onOpen}>{update ? "Update User" : "New User"}</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay/>
+          <ModalContent width="500px" boxShadow={"0px 4px 30px 0px #00000040"}>
+            <ModalHeader>Account details</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              {/* <form onSubmit={handleSubmit}> */}
+                <FormControl>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    placeholder={name}
+                    onChange={(input) => setName(input.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Avatar</FormLabel>
+                  <Input
+                    placeholder={avatar}
+                    onChange={(event) => setAvatar(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Phone</FormLabel>
+                  <Input
+                    placeholder={phone}
+                    onChange={(event) => setPhone(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    placeholder={email}
+                    onChange={(event) => setEmail(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Rating</FormLabel>
+                  <Input
+                    placeholder={rating}
+                    onChange={(event) => setRating(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Status</FormLabel>
+                  <Input
+                    placeholder={status ? "active" : "inactive"}
+                    onChange={(event) => setStatus(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Hero Project</FormLabel>
+                  <Input
+                    placeholder={project}
+                    onChange={(event) => setProject(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <Button colorScheme="blue" mr={3} type="submit" onClick = {()=>{onClose();handleSubmit()}}>
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+            </ModalBody>
+
+            <ModalFooter></ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
